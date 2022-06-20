4b20cc93ce8d0b005916933344a896afeb97b1c2
kumarReddy8686 <arashokkumar55@gmail.com>
2022-06-20 11:53:33 +0530

    my first commit

---
+++ src/basics/StringSwapping.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class StringSwapping {
+
+	public static void main(String[] args) {
+		String x, y, temp;
+
+		System.out.println("Enter x and y:");
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextLine();
+		y = sc.nextLine();
+
+		System.out.println("before swapping:" + "\nx=" + x + "\ny=" + y);
+		temp = x;
+		x = y;
+		y = temp;
+		System.out.println("after swapping:" + "\nx=" + x + "\ny=" + y);
+
+		sc.close();
+
+	}
+
+}
